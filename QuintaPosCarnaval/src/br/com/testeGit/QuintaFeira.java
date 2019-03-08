9647c57dee8237d6d934e9848a137dad8070ba48
jeferson <jefersonqueiroga@gmail.com>
2019-03-07 21:00:33 -0300

    Versao da tarde. Alteracoes

--- QuintaPosCarnaval/src/br/com/testeGit/QuintaFeira.java
+++ QuintaPosCarnaval/src/br/com/testeGit/QuintaFeira.java
@@ -1,7 +1,13 @@
 package br.com.testeGit;
 
+/**Comentarioas
+ * 
+ * @author jeferson
+ *
+ */
 public class QuintaFeira {
 	public static void main(String[] args) {
-		
+		System.out.println("Testando");
+			
 	}
 }
