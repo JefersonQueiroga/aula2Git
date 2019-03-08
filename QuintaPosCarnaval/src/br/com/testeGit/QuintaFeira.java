1063d903a8053561e81e3392918f237aeee6a50c
moize <moize@10.112.4.20>
2019-03-07 21:35:40 -0300

    Teste de Commit

--- QuintaPosCarnaval/src/br/com/testeGit/QuintaFeira.java
+++ QuintaPosCarnaval/src/br/com/testeGit/QuintaFeira.java
@@ -18,4 +18,8 @@ public class QuintaFeira {
 	public static void metodoMauricio(){
 		System.out.println("cheguei também nessa bagaça!!");
 	}
+	
+	public static void metodoMoises(){
+		System.out.println("Ainda tô na bagaça");
+	}
 }
