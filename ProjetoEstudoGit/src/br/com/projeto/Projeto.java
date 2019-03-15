f9c5bf7939ead8e6489aa7f1f38eaf62531623c8
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-03-14 21:10:46 -0300

    commit inicial do projeto

---
+++ ProjetoEstudoGit/src/br/com/projeto/Projeto.java
@@ -0,0 +1,15 @@
+package br.com.projeto;
+
+
+
+public class Projeto {
+	public static void main(String[] args) {
+		System.out.println("Testando...");
+		
+	}
+	
+	public static void metodoRobson(){
+		System.out.println("cheguei nessa baga�a!!");
+	}
+
+}
